51668abcf70133ac083d0387f732c27e481ee841
DP1NPeresunko <>
2021-06-01 11:20:08 +0000

    Files

---
+++ README.md
@@ -0,0 +1,16 @@
+# Autonoma
+
+## Projekta nosaukums: 
+ZCars
+
+## Grupa: 
+DP1-1
+7.grupa 
+
+## Autori: 
+Aleksandrs Loceks,
+Daniils Rudijs,
+Nikita Peresunko
+
+## FIGMA Saite: 
+https://www.figma.com/file/NiLrG4q0brtSUo0FV1DJD3/Untitled?node-id=0%3A1
--- main.py
+++ main.py
@@ -3,12 +3,24 @@ app = Flask('app')
 
 
 @app.route('/')
-def index():
+def galvena():
   return render_template("galvena.html")
 
 @app.route('/kontakti')
-def about():
+def kontakti():
   return render_template("kontakti.html")
+
+@app.route('/parasta zona')
+def parasta_zona():
+  return render_template("parasta zona.html")
+
+@app.route('/rezervēšana')
+def rezervēšana():
+  return render_template("rezervēšana.html")
+
+@app.route('/vip')
+def vip():
+  return render_template("vip.html")
 
 
 app.run(host='0.0.0.0', port=8080)
